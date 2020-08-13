f69ee9f4815a24ddca342f8118e00dc14f120a41
Matt Pilott <matt@creativelittledots.co.uk>
2020-08-13 16:18:50 +0100

    tidy

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,14 +3,12 @@
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Sapper DashJS test</title>
 </svelte:head>
 
 <h1>Dash test</h1>
 
 <video playsinline autoplay muted loop bind:this="{video}"></video>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
 
 <script>
 	import dashjs from 'dashjs';
@@ -20,6 +18,6 @@
 
 	onMount(() => {
 		let player = dashjs.MediaPlayer().create();
-         player.initialize(video, `https://videodelivery.net/6eacc1e5e33739494095da1827437f4b/manifest/video.mpd`, true);
+      player.initialize(video, `https://videodelivery.net/6eacc1e5e33739494095da1827437f4b/manifest/video.mpd`, true);
 	})
 </script>
